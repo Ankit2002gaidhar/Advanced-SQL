c0ec65871e070e0f71abce4d1d169206bb73367f
Ankit Gaidhar <115390294+Ankit2002gaidhar@users.noreply.github.com>
2024-08-19 20:45:26 +0530

    Update Date and time Functions.sql

--- Date and time Functions.sql
+++ Date and time Functions.sql
@@ -107,3 +107,71 @@ from Sales.Orders
 where Month(OrderDate)=2;
 
 -- Avoid using DateName for filtering data ,use datepart
+
+
+-- Format() function
+
+Select 
+OrderID,
+CreationTime,
+Format(CreationTime,'MM-dd-yyy') USA_Format,
+Format(CreationTime,'dd-MM-yyy') USA_Format,
+Format(CreationTime,'dd') dd,
+Format(CreationTime,'ddd') ddd,
+Format(CreationTime,'dddd') dddd,
+Format(CreationTime,'MM') MM,
+Format(CreationTime,'MMM') MMM,
+Format(CreationTime,'MMMM') MMMM
+from Sales.Orders;
+
+
+-- Show creation time using the following format:
+-- Day wed Jan Q1 2025 12:34:56 PM
+
+Select 
+OrderID,
+CreationTime,
+'Day '+ Format(CreationTime,'ddd MMM')  +
+' Q' + DateName(quarter,CreationTime) +' '+
+Format(CreationTime, 'yyyy hh:mm:ss tt') as CustomFormat
+from Sales.Orders;
+
+
+-- Use case format() Data Aggregation
+
+Select 
+FORMAT(OrderDate,'MMM yy' ) OrderDate,
+count(*)
+from Sales.Orders
+group by Format(OrderDate,'MMM yy')
+
+--Use case2 : Data Standarddization
+
+
+-- Convert() Function
+
+Select 
+Convert(Int,'123') as [String to Int Convert],
+Convert(Date,'2025-08-20') as [String to Date Convert],
+CreationTime,
+Convert(Date,CreationTime) as [Datetime to Date Convert]
+from Sales.Orders;
+
+
+Select 
+CreationTime,
+Convert(Date,CreationTime) as [Datetime to Date Convert],
+Convert(Varchar,CreationTime,32) as [USA Std. Style:32],
+Convert(Varchar,CreationTime,34) as [EURO Std. Style:34]
+from Sales.Orders;
+
+
+-- Cast() 
+Select 
+Cast('123' as INT) as [String to Int],
+Cast(123 as varchar) as [Int to String],
+Cast('2025-08-20' as Date) as [String to Date],
+Cast('2025-08-20' as DateTime2) as [String to Datetime],
+CreationTime,
+Cast(CreationTime as Date) as [DateTime to Date]
+from Sales.Orders;
